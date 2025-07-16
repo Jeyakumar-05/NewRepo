88b9047fa09cb49f914ba1fe85fc2a0b98cea221
Jeyakumar S <132358866+Jeyakumar-05@users.noreply.github.com>
2025-07-16 16:49:02 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
-import Layout from './components/layout/Layout'
+
 import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
 import NotFoundPage from './pages/NotFoundPage'
