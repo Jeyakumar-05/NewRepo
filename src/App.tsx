95f5617b708eba437f13d49beb86467c3c0ce23b
Jeyakumar S <132358866+Jeyakumar-05@users.noreply.github.com>
2025-07-16 15:49:47 +0530

    Updated branch NewBranch via App Studio

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Layout from './components/layout/Layout'
 import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
 import NotFoundPage from './pages/NotFoundPage'
-//changes in local branch
+
 function App() {
   return (
     <Routes>
